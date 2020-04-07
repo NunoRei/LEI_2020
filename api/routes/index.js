96ab4bd3517bbc2ea212390c51d3b96043f02e5a
Fábio Senra <fabio-senra@hotmail.com>
2020-04-07 15:43:21 +0100

    commit not working

--- api/routes/index.js
+++ api/routes/index.js
@@ -17,6 +17,22 @@ router.get('/meds',(req,res) =>{
     })
 })
 
+router.post('/newUtente',(req,res)=>{
+  nome=req.body.nome
+  nUtente=req.body.nUtente
+  sexo=req.body.sexo
+  data=req.body.data
+
+  console.log(data)
+
+  Database.insertUtente(nome,sexo,data,nUtente)
+    .then(dados => {
+      res.jsonp(dados)
+    })
+    .catch(erro => {
+      res.jsonp(erro)
+    })
+})
 
 
 module.exports = router;
