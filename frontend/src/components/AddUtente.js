6a14aeb998050bdb04fd1da00ed00e0d56ceb7cd
Fábio Senra <fabio-senra@hotmail.com>
2020-04-22 23:20:52 +0100

    inserir utente bonito

--- frontend/src/components/AddUtente.js
+++ frontend/src/components/AddUtente.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import {Redirect} from 'react-router-dom';
+import Profilepic from '../1.jpg'
 
 class AddUtente extends Component {
     constructor() 
@@ -65,41 +66,51 @@ class AddUtente extends Component {
             return <Redirect to="/utente"/>
         }else {
             return (
-                <form onSubmit={this.handleOnSubmit}>
-                    <label>Nome</label>
-                    <input class="w3-input w3-round-large"
-                        type="text"
-                        value={this.state.Name}
-                        name="Name"
-                        placeholder="Nome do Utente"
-                        onChange={this.handleChange}
-                    />
-                    <label>Número do Utente</label>
-                    <input class="w3-input w3-round-large"
-                        type="text"
-                        value={this.state.Number}
-                        name="Number"
-                        placeholder="Número do Utente"
-                        onChange={this.handleChange}
-                    />
-                    <label>Data de nascimento do Utente</label>
-                    <input class="w3-input w3-round-large"
-                        type="date"
-                        value={this.state.Birth}
-                        name="Birth"
-                        placeholder=""
-                        onChange={this.handleChange}
-                    />
-                    <label>Género</label>
-                    <input class="w3-input w3-round-large"
-                        type="text"
-                        value={this.state.Sex}
-                        name="Sex"
-                        placeholder="Género do utente"
-                        onChange={this.handleChange}
-                    />
-                    <button onClick={this.handleOnSubmit} class="w3-button w3-white w3-border w3-border-blue w3-round-large w3-hover-light-blue">Adicionar Utente</button>
-                </form>
+
+                <div class="w3-row">
+                    <div class="w3-quarter " > <br/></div>
+                    <div class="w3-half " >
+                        
+                        <form class="w3-padding w3-card" onSubmit={this.handleOnSubmit} >
+                            <h2 class="w3-center">Novo Utente</h2>
+                            <br></br>
+                            <label class="w3-text-black">Nome</label>
+                            <input class="w3-input w3-round-large"
+                                type="text"
+                                value={this.state.Name}
+                                name="Name"
+                                placeholder="Nome do Utente"
+                                onChange={this.handleChange}
+                            />
+                            <label>Número</label>
+                            <input class="w3-input w3-round-large"
+                                type="text"
+                                value={this.state.Number}
+                                name="Number"
+                                placeholder="Número do Utente"
+                                onChange={this.handleChange}
+                            />
+                            <label>Data de nascimento</label>
+                            <input class="w3-input w3-round-large"
+                                type="date"
+                                value={this.state.Birth}
+                                name="Birth"
+                                placeholder=""
+                                onChange={this.handleChange}
+                            />
+                            <label>Género</label>
+                            <input class="w3-input w3-round-large"
+                                type="text"
+                                value={this.state.Sex}
+                                name="Sex"
+                                placeholder="Género do utente"
+                                onChange={this.handleChange}
+                            />
+                            <button onClick={this.handleOnSubmit} class="w3-button w3-white w3-border w3-border-blue w3-round-large w3-hover-blue">Adicionar Utente</button>
+                        </form>
+                    </div>
+                    <div class="w3-quarter" ></div>
+                </div>
             )
         }
         }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,10 +5,10 @@ function Header()
 {
     return (
         <div class="w3-row" >
-            <div class="w3-col s1 w3-center">
+            <div class="w3-col s1 w3-center w3-padding-64">
                 <Sidebar/>
             </div>
-            <div class="w3-col s9 w3-center">
+            <div class="w3-col s9 w3-center w3-padding-64">
                 <h1>Drug Interaction</h1>
             </div>
       </div>
