af7ee1593a314dd4e4c8eec7179b2d35ff5edf09
NunoRei <36609841+NunoRei@users.noreply.github.com>
2020-04-29 23:44:35 +0100

    Order by Severity (high)

--- frontend/src/components/Interactions.js
+++ frontend/src/components/Interactions.js
@@ -5,6 +5,8 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import WarningIcon from '@material-ui/icons/Warning';
 import { ListItemIcon } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
+
 
 class Interactions extends Component {
     constructor() 
@@ -19,6 +21,17 @@ class Interactions extends Component {
     }
 
 
+    /*orderInt(inters) 
+    {
+        var highSev = [] 
+
+        inters.map(inter => {
+            if (inter.interactionPair[0].severity == "high")
+                inters.remove(inter)
+                highSev.push(inter)
+        })
+    }*/
+
     render() 
     {
 
@@ -27,7 +40,16 @@ class Interactions extends Component {
                 <Paper elevation={3}>
                     <h3 class="w3-center">Interactions</h3>
                     <List>
-                        {this.props.value.map(inter => 
+                        {this.props.value.sort(function(a,b) {
+                            if(a.interactionPair[0].severity === "high" && b.interactionPair[0].severity === "high") 
+                                return 0;
+                            else if(a.interactionPair[0].severity === "high")
+                                return -1;
+                            else if(b.interactionPair[0].severity === "high")
+                                return 1;
+                            else 
+                                return 0;
+                        }).map(inter => 
                             {
                                 return (
                                 <ListItem alignItems="flex-start">
