5d45b6cd465cbd140cbb3a85b71188e736bb50ef
Rafaela <rpinho897@gmail.com>
2020-07-01 17:56:14 +0100

    Icons - card.js

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,14 +1,22 @@
 import React from 'react';
-
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function MediaCard(props){
 
 
   return (
-    <div class="card">
+    <div class="card" >
         <h3>{props.name}</h3>
         {props.text.map(frase =>(<p>{frase}</p>))}
-        {props.ligacoes.map(ref =>(<p><a href={ref}>{ref}</a></p>))}
+        <p style={{textAlign: 'center'}}>
+          <a href={props.ligacoes[0]}>
+            <GitHubIcon />
+          </a>
+          <a href={props.ligacoes[1]}>
+            <LinkedInIcon />
+          </a>
+        </p>
     </div>
   );
 }
